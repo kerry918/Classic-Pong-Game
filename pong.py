10fd976e8b23080b4f71a1b63b8e17cefdd550fd
Kerry Liu <k269liu@uwaterloo.ca>
2020-04-22 15:23:34 -0400

    Update pong.py

--- pong.py
+++ pong.py
@@ -1,3 +1,15 @@
 # Classic Pong Game by Kerry Liu
 
-import turtle
+import turtle #could do some basic graphics
+
+# Create a window for the game
+win = turtle.Screen()
+win.title("Pong game by Kerry Liu") #Create a title for the game
+win.bgcolor("black") # Change the background color
+win.setup(width=800, height=600) # Set up the dimensions of the window
+win.tracer(0) # Stops the window from updating to speed up the game
+
+
+# Main game loop
+while True:
+    
